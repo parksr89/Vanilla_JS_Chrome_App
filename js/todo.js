eba35196e604915df366581208b0c84ce5ff9815
parksr89 <54193969+parksr89@users.noreply.github.com>
2023-09-28 21:56:33 -0700

    update

--- js/todo.js
+++ js/todo.js
@@ -23,10 +23,10 @@ function paintTodo(newTodo){
     const span = document.createElement("span");
     span.innerText= newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.innerText = "✔";
     button.addEventListener("click",deleteToDo);
-    li.appendChild(span);
     li.appendChild(button);
+    li.appendChild(span);
     toDolist.appendChild(li);
 }
 
@@ -52,7 +52,7 @@ const savedToDos =localStorage.getItem(TODOS_KEY);
 if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    parsedToDos.forEach(paintTodo);
+    parsedToDos.forEach(paintTodo);    
 }
 
 
